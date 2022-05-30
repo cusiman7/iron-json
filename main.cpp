fb9ae240da146cd4c782d34b5b5c8c1b71df6a64
Rob Cusimano <cusiman7@gmail.com>
2022-05-29 18:20:59 -0700

    Main reads files now

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 
 #include "json.h"
 #include <iostream>
+#include <fstream>
 #include <vector>
 
-int main() {
+std::string read_file(const char* path) {
+    if(std::ifstream is{path, std::ios::binary | std::ios::ate}) {
+        auto size = is.tellg();
+        std::string str(size, '\0'); // construct string to stream size
+        is.seekg(0);
+        if(is.read(&str[0], size)) {
+            return str;
+        }
+    }
+    return {};
+}
+
+int main(int argc, const char** argv) {
+    if (argc > 1) {
+        std::optional<json> j = parse(read_file(argv[1]));
+        if (j) {
+            std::cout << *j << "\n";
+        } else {
+            std::cout << "nope\n";
+        }
+        return 0;
+    }
+
     json null;
     for (auto& it : null) {
         std::cout << it << "\n";
